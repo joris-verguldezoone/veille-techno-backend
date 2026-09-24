beadc08df8382c05987bd49b567584379ad4cb6b
joris-verguldezoone <joris-verguldezoone@laplateforme.io>
2026-09-24 17:05:30 +0200

    feat(auth): create user entity

--- docs/commands.md
+++ docs/commands.md
@@ -1,6 +1,6 @@
 mvn quarkus:add-extension -Dextensions="quarkus-smallrye-openapi"
 openapi : localhost:8080/swagger-ui
-
+mvn quarkus:add-extension -Dextensions="quarkus-rest-jackson"
 ### Git
 git config pull.rebase false
 
@@ -11,3 +11,13 @@ cmd + shit + p:  Java: Update Project
 
 ### Terminal Java & mvn
 mvn clean compile
+
+### Références
+[Quarkus datasource pgsql](https://quarkus.io/guides/datasource/)
+[Quarkus hibernate + panache & dependances](https://quarkus.io/guides/hibernate-orm-panache/)
+[Quarkus installation](https://quarkus.io/guides/getting-started/)
+[Quarkus analogie spring boot](https://quarkus.io/guides/cdi/)
+[Quarkus API REST](https://quarkus.io/guides/rest/)
+[hibernate](https://hibernate.org/)
+[hibernate repository (apparement le css c'est pas pour les dev)](https://jakarta.ee/specifications/data/1.0/apidocs/jakarta.data/module-summary.html)
+[hibernate tags](https://docs.hibernate.org/orm/6.6/javadocs/org/hibernate/boot/model/naming/ImplicitNamingStrategy.html)
--- src/main/java/org/acme/auth/entity/User.java
+++ src/main/java/org/acme/auth/entity/User.java
@@ -1,25 +1,42 @@
 package org.acme.auth.entity;
 
-import java.time.LocalDate;
-import java.util.List;
-import jakarta.persistence.Entity;
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import java.time.LocalDateTime;
 
-@Entity
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity; // Id auto, écriture raccourcie
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity; // @Entity
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table; // @Table
+
+@Entity // référencer la table sur postgres et sur swagger
+@Table(name = "users")
 public class User extends PanacheEntity {
+ // pas besoin de référencer l'id, Panache le fait tout seul 
+
+    @Column(unique = true) // L'email ne pourra etre enregistré qu'une seule fois
+    public String email;
+    
+    @JsonIgnore // pour ne pas renvoyer de mdp lors des @Get
+    // Fonctionne a l'aide du packet quarkus-rest-jackson, pour renvoyer du json
+    @Column(nullable = false)
+    public String password; // truc spécial pour le mdp
+
     public String name;
-    public LocalDate birth;
-    public Status status;
 
-    public static User findByName(String name){
-        return find("name", name).firstResult();
-    }
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    public Role role = Role.USER; // faudra créer l'entité Role
 
-    public static List<User> findAlive(){
-        return list("status", Status.Alive);
-    }
+    @CreationTimestamp 
+    @Column(updatable = false)
+    public LocalDateTime createdAt; // Faudra trouver l'import datetimeLocal
 
-    public static void deleteStefs(){
-        delete("name", "Stef");
+    public enum Role { // Pour l'instant je garde l'enum car y'a que deux roles dansle sujet
+        USER, ADMIN
     }
 }
